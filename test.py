49fb5ce7cc6e4f48783e5e06af68f48721f4360a
mking05 <nyntofive@comcast.net>
2021-01-11 10:08:18 -0500

    Laptop -> PC

---
+++ test.py
@@ -0,0 +1,6 @@
+def test_homepage():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.template.name == 'index.html'
+    assert "request" in response.context
