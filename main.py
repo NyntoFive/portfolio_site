e0fa7f9bf9fffbab4fa667a1b442106b09f5f4c7
Nyntofive <nyntofive@gmail.com>
2021-01-15 16:28:58 -0500

    moar templates

--- main.py
+++ main.py
@@ -33,19 +33,24 @@ async def get_query_token(token: str):
 # async def read_user(username: str):
 #     return {"username": username}
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("home.html", {"request": request, "id": id})
+# @app.get("/items/{id}", response_class=HTMLResponse)
+# async def read_item(request: Request, id: str):
+#     return templates.TemplateResponse("home.html", {"request": request, "id": id})
 
-@app.get("/canvas/")
-async def canvas(request: Request):
-    return templates.TemplateResponse("canvasPage.html",{"request": request, "id": id})
+# @app.get("/canvas/")
+# async def canvas(request: Request):
+#     return templates.TemplateResponse("canvasPage.html",{"request": request, "id": id})
+
 @app.get("/c/")
 async def can(request: Request):
     return templates.TemplateResponse("home.html",{"request": request, "id": id})
+
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html",{"request": request, "id": id})
 @app.get("/insta/")
 async def instagram_clone(request: Request):
     return templates.TemplateResponse("instagram_clone.html", {"request": request, "id": id})
+@app.get("/shop/")
+async def instagram(request: Request):
+    return templates.TemplateResponse("showgrow.html", {"request": request, "id": id})
